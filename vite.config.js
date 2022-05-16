b6119dc05a4bf7d0c93d223e8e794c29f439c1b2
cotton123236 <cotton123236@gmail.com>
2022-05-16 22:41:08 +0800

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dotenv from 'dotenv'
-
-dotenv.config({ path: './../config.env' })
-// console.log(process.env.PORT)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: './../public'
-  }
-  // server: {
-  //   proxy: {
-  //     '/api': `http://localhost:${process.env.PORT}/`,
-  //   },
-  // }
+  plugins: [vue()]
 })
