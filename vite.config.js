55ee7494649ec0914e4202568c46f95f7caaebc6
charlie50503 <cate50503@gmail.com>
2023-01-21 13:31:17 +0800

    feat: [設定檔] 添加 ＠ 可以指定src/底下功能

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
